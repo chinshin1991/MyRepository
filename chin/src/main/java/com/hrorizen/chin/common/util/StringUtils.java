4f2d4d59e64fd297b17634f31df0ada2e0bc9882
chinshin1991 <chinshin1991@gmail.com>
2018-06-17 16:50:43 +0800

    add string isnull method

--- chin/src/main/java/com/hrorizen/chin/common/util/StringUtils.java
+++ chin/src/main/java/com/hrorizen/chin/common/util/StringUtils.java
@@ -1,5 +1,18 @@
 package com.hrorizen.chin.common.util;
 
+/**
+ * @author chenxin
+ * 字符通过用处理类
+ */
 public class StringUtils {
 
+	/**
+	 * 判断字符串是否为空
+	 * @param str 待判定字符串
+	 * @return true 字符串为空  false 字符串不为空
+	 */
+	public static boolean isNull(String str) throws Exception{
+		if( str == null || "".equals(str )) return true;
+		return false;
+	}
 }
