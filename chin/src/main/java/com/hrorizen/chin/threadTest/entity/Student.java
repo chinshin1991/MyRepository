abe056f8074efc39cca270d40b1bb3630a2b8898
chinshin1991 <chinshin1991@gmail.com>
2018-06-17 16:20:36 +0800

    test student class

---
+++ chin/src/main/java/com/hrorizen/chin/threadTest/entity/Student.java
@@ -0,0 +1,48 @@
+package com.hrorizen.chin.threadTest.entity;
+
+/**
+ * @author chenxin
+ * 学生类 
+ */
+public class Student {
+	
+	/**
+	 * 姓名
+	 */
+	private String name;
+	
+	/**
+	 * 年龄
+	 */
+	private int age;
+	
+	/**
+	 * 性别
+	 */
+	private String gender;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
