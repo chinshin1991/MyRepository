642362c7953413e1e5a43b5a701c4de3474baa60
chinshin1991 <chinshin1991@gmail.com>
2018-06-17 16:36:13 +0800

    commit

--- chin/src/main/java/com/hrorizen/chin/App.java
+++ chin/src/main/java/com/hrorizen/chin/App.java
@@ -4,10 +4,8 @@ package com.hrorizen.chin;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
---
+++ chin/src/main/java/com/hrorizen/chin/common/constants/Gender.java
@@ -0,0 +1,17 @@
+package com.hrorizen.chin.common.constants;
+
+/**
+ * @author chenxin
+ * 性别枚举类
+ */
+public enum Gender {
+	/**
+	 *  女
+	 */
+	MALE ,
+	
+	/**
+	 *  男
+	 */
+	FEMALE
+}
